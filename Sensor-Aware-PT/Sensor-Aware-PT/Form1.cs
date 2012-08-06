0b6908f626fad16d64859c6aff86a2dde2e5a263
Saul D Beniquez <sdbenique@gmail.com>
2012-08-05 21:56:44 -0400

    Second commit. Forgot to add the files to git. Woops.

---
+++ Sensor-Aware-PT/Sensor-Aware-PT/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace Sensor_Aware_PT
+{
+    public partial class Form1 : Form
+    {
+        bool loaded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded)
+                return;
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded)
+                return;
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            glControl1.SwapBuffers();
+        }
+    }
+}
