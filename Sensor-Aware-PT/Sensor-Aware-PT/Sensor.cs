cba9031e67155d5193286d820eaf330ee8f69f58
Mojo Jojo <mofidul2@gmail.com>
2012-08-08 13:07:04 -0400

    Initial stuff

--- Sensor-Aware-PT/Sensor-Aware-PT/Sensor.cs
+++ Sensor-Aware-PT/Sensor-Aware-PT/Sensor.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Sensor_Aware_PT
 {
+    /** Created to encapsulate a sensor */
     class Sensor
     {
+        /** Friendly sensor ID A-D */
+        private string mSensorID;
+        /** Last 4 Digits of mac address */
+        private string mSensorMAC;
+        /** Holds a history of previous positions */
+        private List<OpenTK.Vector3> mDataHistory;
+
+
     }
 }
