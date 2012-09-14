4cd647df2ff59904f8d7e04a574c62425d7fc081
bobomb <mofidul2@gmail.com>
2012-09-14 11:42:01 -0400

    One skeleton class only, no descendents

--- Sensor-Aware-PT/Sensor-Aware-PT/Skeleton.cs
+++ Sensor-Aware-PT/Sensor-Aware-PT/Skeleton.cs
@@ -14,16 +14,48 @@ namespace Sensor_Aware_PT
         LegUpperL, LegUpperR,
 
         BackLower, BackUpper,
-        Neck
+        Neck,
+        ShoulderL, ShoulderR
     }
 
-    public abstract class Skeleton
+    public enum SkeletonType
     {
-        private List<Bone> mBones = new List<Bone>();
-        private Dictionary<String, Bone> mBoneMapping = new Dictionary<String, Bone>();
-        
-        public abstract void draw();
-        public virtual void update( SensorDataEntry data )
+        UpperBody,
+        LowerBody,
+        MidBody,
+        Dog,
+        TRex
+    }
+    public class Skeleton
+    {
+        protected List<Bone> mBones = new List<Bone>();
+        protected Dictionary<String, Bone> mBoneMapping = new Dictionary<String, Bone>();
+        protected Dictionary<BoneType, float> mBoneLengthMapping = new Dictionary<BoneType, float>();
+
+        public Skeleton()
+        {
+            // Default bone length mappings
+            mBoneLengthMapping.Add(BoneType.BackUpper, 5f);
+            mBoneLengthMapping.Add(BoneType.BackLower, 3f);
+            mBoneLengthMapping.Add(BoneType.ArmLowerL, 2f);
+            mBoneLengthMapping.Add(BoneType.ArmLowerR, 2f);
+            mBoneLengthMapping.Add(BoneType.ArmUpperL, 2f);
+            mBoneLengthMapping.Add(BoneType.ArmUpperR, 2f);
+            mBoneLengthMapping.Add(BoneType.LegLowerL, 2.5f);
+            mBoneLengthMapping.Add(BoneType.LegLowerR, 2.5f);
+            mBoneLengthMapping.Add(BoneType.LegUpperL, 2.5f);
+            mBoneLengthMapping.Add(BoneType.LegUpperR, 2.5f);
+            mBoneLengthMapping.Add(BoneType.ShoulderL, 1f);
+            mBoneLengthMapping.Add(BoneType.ShoulderR, 1f);
+        }
+
+        public Skeleton(SkeletonType skelType)
+        {
+
+        }
+        public void draw();
+
+        public void update( SensorDataEntry data )
         {
             Bone mappedBone;
             if(mBoneMapping.TryGetValue(data.id, out mappedBone))
@@ -35,6 +67,6 @@ namespace Sensor_Aware_PT
                 //?
             }
         }
-        public abstract void createMapping( string sensorID, BoneType mapping );
+        public  void createMapping( string sensorID, BoneType mapping );
     }
 }
