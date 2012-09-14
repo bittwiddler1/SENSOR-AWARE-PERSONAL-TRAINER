2f5f3ca27b4a42d977972dd01aa51b0e5bf97ae7
bobomb <mofidul2@gmail.com>
2012-09-14 12:37:19 -0400

    more bone

--- Sensor-Aware-PT/Sensor-Aware-PT/Skeleton.cs
+++ Sensor-Aware-PT/Sensor-Aware-PT/Skeleton.cs
@@ -51,6 +51,68 @@ namespace Sensor_Aware_PT
 
         public Skeleton(SkeletonType skelType)
         {
+            switch (skelType)
+            {
+                case SkeletonType.UpperBody:
+                    createUpperBody();
+                    break;
+                case SkeletonType.LowerBody:
+                    createLowerBody();
+                    break;
+                case SkeletonType.MidBody:
+                    createMidBody();
+                    break;
+                case SkeletonType.Dog:
+                    break;
+                case SkeletonType.TRex:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void createMidBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void createLowerBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void createUpperBody()
+        {
+            Bone Back, ArmUL, ArmUR, ArmLL, ArmLR, ShoulderL, ShoulderR;
+            ArmUL = new Bone(mBoneLengthMapping[BoneType.ArmUpperL], new Vector3());
+            ArmUR = new Bone(mBoneLengthMapping[BoneType.ArmUpperR], new Vector3());
+            ArmLL = new Bone(mBoneLengthMapping[BoneType.ArmLowerL], new Vector3());
+            ArmLR = new Bone(mBoneLengthMapping[BoneType.ArmLowerR], new Vector3());
+            ShoulderL = new Bone(mBoneLengthMapping[BoneType.ShoulderL], new Vector3());
+            ShoulderR = new Bone(mBoneLengthMapping[BoneType.ShoulderR], new Vector3());
+
+            // Set the orientations accordingly~
+            
+            /** Back */
+            Back.addChild(ShoulderL);
+            Back.addChild(ShoulderR);
+            /** Shoulders */
+            ShoulderR.updateOrientation(new Vector3());
+            ShoulderL.updateOrientation(new Vector3());
+            /** Left Arm upper */
+            ShoulderL.addChild(ArmUL);
+            ArmUL.updateOrientation(new Vector3());
+            /** Right arm upper */
+            ShoulderR.addChild(ArmUR);
+            ArmUR.updateOrientation(new Vector3());
+            /** Left arm lower */
+            ArmUL.addChild(ArmLL);
+            ArmLL.updateOrientation(new Vector3());
+            /** Right arm lower */
+            ArmUR.addChild(ArmLR);
+            ArmLR.updateOrientation(new Vector3());
+
+            
 
         }
         public void draw();
