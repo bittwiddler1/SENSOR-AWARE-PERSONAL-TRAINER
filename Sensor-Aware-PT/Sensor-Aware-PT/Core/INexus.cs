b0962a62be84b0a371b5354b31bc13fadea818cd
bobomb <mofidul2@gmail.com>
2012-09-24 17:29:51 -0400

    Added INexus stub

---
+++ Sensor-Aware-PT/Sensor-Aware-PT/Core/INexus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sensor_Aware_PT
+{
+    /// <summary>
+    /// Interface used to facilitate communication with the Nexus
+    /// </summary>
+    public interface INexus
+    {
+
+    }
+}
